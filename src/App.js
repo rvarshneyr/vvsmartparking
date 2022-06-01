0a9f3979baf10708ae55916682bb46a630c133cf
Varshney <varshney@splm.siemens.com>
2022-06-01 17:12:23 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -8,8 +8,18 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import {API, Auth} from 'aws-amplify'
+
 
 function App({ signOut }) {
+  async function callAPi () {
+    const user = await Auth.currentAuthenticatedUser()
+    const token = user.signInUserSession.idToken.jwtToken
+
+    const data = await API.get ('', '/hello', )
+    console.log(data)
+
+  }
   return (
     <View className="App">
       <Card>
@@ -17,6 +27,7 @@ function App({ signOut }) {
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
+      <Button onClick={callAPi}> Find nearest parking</Button>
     </View>
   );
 }
